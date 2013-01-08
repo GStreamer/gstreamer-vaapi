6ec3d9d8fee2141a0d1c21f001089b390f8bc51a
Zhao Halley <halley.zhao@intel.com>
2013-01-09 05:40:46 +0800

    h264: reset got_{sps,pps} when corrupted SPS/PPS headers are parsed.
    Reset got_{sps,pps} when corrupted SPS/PPS headers are parsed. This means
    that no frame will be decoded until valid SPS/PPS headers are parsed again.
    This also prevents from parsing slice headers with invalid data, and avoids
    potential crashes.
    
    https://bugs.freedesktop.org/show_bug.cgi?id=57902

--- gst-libs/gst/vaapi/gstvaapidecoder_h264.c
+++ gst-libs/gst/vaapi/gstvaapidecoder_h264.c
@@ -1175,8 +1175,10 @@ decode_sps(GstVaapiDecoderH264 *decoder, GstH264NalUnit *nalu)
 
     memset(sps, 0, sizeof(*sps));
     result = gst_h264_parser_parse_sps(priv->parser, nalu, sps, TRUE);
-    if (result != GST_H264_PARSER_OK)
+    if (result != GST_H264_PARSER_OK) {
+        priv->got_sps = FALSE;
         return get_status(result);
+    }
 
     priv->got_sps = TRUE;
     return GST_VAAPI_DECODER_STATUS_SUCCESS;
@@ -1193,8 +1195,10 @@ decode_pps(GstVaapiDecoderH264 *decoder, GstH264NalUnit *nalu)
 
     memset(pps, 0, sizeof(*pps));
     result = gst_h264_parser_parse_pps(priv->parser, nalu, pps);
-    if (result != GST_H264_PARSER_OK)
+    if (result != GST_H264_PARSER_OK) {
+        priv->got_pps = FALSE;
         return get_status(result);
+    }
 
     priv->got_pps = TRUE;
     return GST_VAAPI_DECODER_STATUS_SUCCESS;
